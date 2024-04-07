d4d4213ef3995567d08bb8a9c9c01ff47b44a0d7
Genewolf <79755741+Genewolf@users.noreply.github.com>
2024-04-07 14:38:04 +0200

    petite erreur de conflit résolue
    ça avait prit la version de Hugo alors que la mienne fonctionnait un peu mieux hehe

--- Projet_Cpp_4A/login.cpp
+++ Projet_Cpp_4A/login.cpp
@@ -22,41 +22,13 @@ login::~login()
  */
 void login::on_ButtonValider_clicked()
 {
-    //On créer 2 utilisateurs pour tester si l'application fonctionne.
-    vector<Profil> profils;
-    User test1 = User("Test", "test1234", "Do", "John", 1, profils);
-    User test2 = User("Test2", "test", "Smith", "James", 1, profils);
-
     //On récupére les lignes de saisie pour le mots de passe et l'identifiant
     QString idLine = ui->idLine->text();
     QString mdpLine = ui->mdpLine->text();
 
-
+    //On verifie que le mdp et le login soit correct
     bool Connexion = false;
     vector<User> listeUsers = globalUserManager.getListeUsers();    //la liste des Users tirées du json
-        //On verifie que le mdp et le login soit correct
-        if (idLine.toStdString() == test1.getLogin() && mdpLine.toStdString() == test1.getMdp()){
-            Connexion = true;
-            hide();
-            UserWindow userwindow = new UserWindow(nullptr);
-            userwindow.setUser(test1.getNom(), test1.getPrenom());
-            userwindow.setModal(true);
-            userwindow.exec();
-        }else{
-            if (idLine.toStdString() == test2.getLogin() && mdpLine.toStdString() == test2.getMdp()){
-                Connexion = true;
-                hide();
-                UserWindow userwindow = new UserWindow(nullptr);
-                userwindow.setUser( test2.getNom(), test2.getPrenom());
-                userwindow.setModal(true);
-                userwindow.exec();
-            }
-        }
-        if(Connexion == false) QMessageBox::warning(this,"Login","Le login n'est pas bon !");
-
-    //On verifie que le mdp et le login soit correct
-
-    /*vector<User> listeUsers = globalUserManager.getListeUsers();    //la liste des Users tirées du json
 
     for(int ItUser=0; ItUser<listeUsers.size(); ItUser++){
         if (idLine.toStdString() == listeUsers[ItUser].getLogin() && mdpLine.toStdString() == listeUsers[ItUser].getMdp()){
@@ -67,7 +39,7 @@ void login::on_ButtonValider_clicked()
             user.setModal(true);
             user.exec();
         }
-
-    }*/
+    }
+    if(Connexion == false) QMessageBox::warning(this,"Login","Le login n'est pas bon !");
 }
 
