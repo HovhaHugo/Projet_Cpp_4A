d741182eae497843073726f8205ac28827fc51d6
Genewolf <79755741+Genewolf@users.noreply.github.com>
2024-04-08 19:42:11 +0200

    Add: UserEditDialog
    fenêtre n'apparaissant que pour les admin permettant la gestion d'utilisateurs

--- Projet_Cpp_4A/main.cpp
+++ Projet_Cpp_4A/main.cpp
@@ -4,6 +4,9 @@
 #include "usermanager.h"
 #include "globals.h"
 
+#include <QCoreApplication>
+#include <QFileInfo>
+
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
@@ -22,8 +25,17 @@ int main(int argc, char *argv[])
             break;
         }
     }
+/*
+    //Permet d'avoir le chemin relatif à la racine du projet (FAIS PLANTER LE PROGRAMME)
+    QCoreApplication app(argc, argv);
+    QString executablePath = QCoreApplication::applicationFilePath();
+    QFileInfo executableInfo(executablePath);
+    QString projectRootPath = executableInfo.absolutePath();
 
-    string PathToJson = "/Users/hugohovhannessian/Hugo/Etude_Sup/Polytech/DI4/S8/PlatLog_Cpp/test2.json";
+    string PathToJson = projectRootPath.toStdString() + "/test.json";
+*/
+    //string PathToJson = "/Users/hugohovhannessian/Hugo/Etude_Sup/Polytech/DI4/S8/PlatLog_Cpp/test2.json";
+    string PathToJson = "C:/Users/benja/OneDrive/Bureau/test/test.json";
 
     globalUserManager.parseFile(PathToJson);      //on récupère les données du json et on les met dans une variable globale
     vector<User> vectorTest = globalUserManager.getListeUsers();
